5bde3627d69e0d7ce9f8e6b7b8adcebab15467e7
IanPol0 <ipolonuer@gmail.com>
2022-05-27 20:58:03 -0300

    Initial commit

---
+++ Assets/Scripts/MovementScriptj1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScriptj1 : MonoBehaviour
+{
+    public float jumpForce;
+
+    bool hasJump = true;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, 0.1f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -0.1f);
+        }
+        if (Input.GetKey(KeyCode.D) && hasJump)
+        {
+            transform.Rotate(0, 5, 0);
+        }
+        if (Input.GetKey(KeyCode.A) && hasJump)
+        {
+            transform.Rotate(0, -5, 0);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && hasJump) //SALTO
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            hasJump = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            hasJump = true;
+        }
+
+        if (col.gameObject.name == "LastGround")
+        {
+            transform.position = new Vector3(-1, 0.5f, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/MovementScriptj2.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScriptj2 : MonoBehaviour
+{
+
+    public float jumpForce;
+
+
+    bool hasJump = true;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, 0.1f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -0.1f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && hasJump)
+        {
+            transform.Rotate(0, 5, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && hasJump)
+        {
+            transform.Rotate(0, -5, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P) && hasJump) //SALTO
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            hasJump = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            hasJump = true; ;
+        }
+        
+        if (col.gameObject.name == "LastGround")
+        {
+            transform.position = new Vector3 (1, 0.5f, 0);
+        }
+    }
+
+}
